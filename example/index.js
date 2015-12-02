4eeeba3ca9b4bf7041db0c9690fd0cdeb98a709d
Ionică Bizău <bizauionica@gmail.com>
2015-12-02 14:15:59 +0200

    initial

---
+++ example/index.js
@@ -0,0 +1,26 @@
+const CompositionCRUD = require("../lib")
+    , EnginePaths = require("engine-paths")
+    ;
+
+// Initialize CRUD
+var crud = new CompositionCRUD(
+    new EnginePaths("path/to/some/app")
+);
+
+// Create the layout instance
+crud.create("layout", /* optional data, */ function (err, data) {
+    console.log(err || data);
+    // => { name: "layout" }
+});
+
+// Read the layout instance
+crud.read("layout", function (err, data) {
+    console.log(err || data);
+    // => { name: "...", ...}
+});
+
+// Read the layout instance
+crud.update("layout", { module: "some-module" }, function (err, data) {
+    console.log(err || data);
+    // => { name: "...", module: "some-module", ...}
+});
