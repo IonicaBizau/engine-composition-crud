549ea41d26b261634f297703966cb74f425052b8
Ionică Bizău <bizauionica@gmail.com>
2015-12-04 12:22:26 +0200

    Testing the crud module

--- test/index.js
+++ test/index.js
@@ -8,4 +8,38 @@ tester.describe("testing the CRUD operations", test => {
     var crud = new CompositionCRUD(
         new EnginePaths(`${__dirname}/demo-app`)
     );
+
+    test.it("create instance", cb => {
+        crud.create("layout", cb);
+    });
+
+    test.it("read instance", cb => {
+        crud.read("layout", (err, data) => {
+            test.expect(err).toBe(null);
+            test.expect(data).toEqual({ name: "layout" });
+            cb();
+        });
+    });
+
+    test.it("update instance", cb => {
+        crud.update("layout", { module: "test" }, cb);
+    });
+
+    test.it("read instance", cb => {
+        crud.read("layout", (err, data) => {
+            test.expect(err).toBe(null);
+            test.expect(data).toEqual({ name: "layout", module: "test" });
+            cb();
+        });
+    });
+
+    test.it("list instances", cb => {
+        crud.list((err, data) => {
+            test.expect(err).toBe(null);
+            test.expect(data).toEqual(["layout"]);
+            cb();
+        });
+    });
+
+    test.it("delete instance", cb => crud.remove("layout", cb));
 });
